11eb20eafb329e6373b1ddba0bef48ddf07204b8
Pauli Kettunen <pauli@sarkain.com>
2024-04-02 11:51:34 +0300

    Add the new foreground service type param

--- android/src/main/java/com/supersami/foregroundservice/ForegroundService.java
+++ android/src/main/java/com/supersami/foregroundservice/ForegroundService.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Handler;
 import android.util.Log;
+import android.content.pm.ServiceInfo;
 
 import com.facebook.react.HeadlessJsTaskService;
 
@@ -79,7 +80,13 @@ public class ForegroundService extends Service {
                 .getInstance(getApplicationContext())
                 .buildNotification(getApplicationContext(), notificationConfig);
 
-            startForeground(id, notification);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                startForeground(id, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
+            }
+            else
+            {
+                startForeground(id, notification);
+            }
 
             running += 1;
 
